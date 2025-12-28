78b299dd3791f9d9aafff4a951e3cf7011cfd47a
Saylee <saylee.waje@gmail.com>
2025-12-27 23:25:58 -0500

    creating review, getting specific review with reviewid company id

--- src/main/java/com/springboot/jobapp/review/ReviewController.java
+++ src/main/java/com/springboot/jobapp/review/ReviewController.java
@@ -1,12 +1,10 @@
 package com.springboot.jobapp.review;
 
 
+import com.springboot.jobapp.company.Company;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,26 @@ public class ReviewController {
     @GetMapping("/reviews")
     public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId){
         return new ResponseEntity<>(reviewService.getAllReviews(companyId), HttpStatus.OK);
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<String> addReview(@PathVariable Long companyId,@RequestBody Review review){
+
+
+        boolean isReviewSaved = reviewService.addReview(companyId,review);
+        if(isReviewSaved){
+            return new ResponseEntity<>("Review added successfully", HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("Review not saved", HttpStatus.NOT_FOUND);
+
+    }
+
+
+    @GetMapping("/reviews/{reviewId}")
+    public  ResponseEntity<Review> getReview(@PathVariable Long companyId, @PathVariable Long reviewId){
+
+
+        return  new ResponseEntity<>(reviewService.getReview(companyId, reviewId), HttpStatus.OK);
     }
 }
--- src/main/java/com/springboot/jobapp/review/impl/ReviewServiceImpl.java
+++ src/main/java/com/springboot/jobapp/review/impl/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.springboot.jobapp.review.impl;
 
+import com.springboot.jobapp.company.Company;
+import com.springboot.jobapp.company.CompanyService;
 import com.springboot.jobapp.review.Review;
 import com.springboot.jobapp.review.ReviewRepository;
 import com.springboot.jobapp.review.ReviewService;
@@ -12,13 +14,36 @@ public class ReviewServiceImpl implements ReviewService {
 
     private ReviewRepository reviewRepository;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    private CompanyService companyService;
+    public ReviewServiceImpl(ReviewRepository reviewRepository, CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findByCompanyId(companyId);
         return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+
+        if(company!=null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }
+
+        return  false;
+
+    }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews.stream().filter(review -> review.getId().equals(reviewId))
+                .findFirst().orElse(null);
     }
 }
